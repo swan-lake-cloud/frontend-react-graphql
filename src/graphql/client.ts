d8b7dfa6254a0444579e5a0b74dc3ca4a551ca5e
Swan RONCO <swan.ronco+amadeus@amadeus.com>
2025-11-18 10:46:20 +0100

    use url in .env

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:8080/jaxrs-liquibase-graphql-api-0.0.1-SNAPSHOT/api/graphql',
+    uri: import.meta.env.VITE_GRAPHQL_URL,
   }),
   cache: new InMemoryCache(),
 })
